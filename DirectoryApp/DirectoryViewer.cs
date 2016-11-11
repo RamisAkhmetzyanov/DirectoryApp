c60d5ea1ffad034a6e9ea5b41629911045142a51
Рамис <@->
2016-11-12 02:10:08 +0300

    Fixed version

--- DirectoryApp/DirectoryViewer.cs
+++ DirectoryApp/DirectoryViewer.cs
@@ -9,7 +9,6 @@ namespace DirectoryApp
     public static class DirectoryViewer
     {
         private static char separator = '|';
-        private static string dateFormat = "yyyy-MM-dd";
 
         public static bool ResolveInput(string input, out string message)
         {
@@ -21,17 +20,15 @@ namespace DirectoryApp
                 return false;
             }
             string RootFolderPath = input.Substring(0, separatorIndex);
-            if (RootFolderPath[RootFolderPath.Length - 1] != '\\')
-                RootFolderPath = RootFolderPath + "\\";
             string command = input.Substring(separatorIndex + 1, input.Length - separatorIndex - 1);
             RootFolderPath = Environment.ExpandEnvironmentVariables(RootFolderPath);
 
-            if (!Directory.Exists(RootFolderPath))
+            DirectoryInfo RootDirectory = new DirectoryInfo(RootFolderPath);
+            if (!RootDirectory.Exists)
             {
                 try
                 {
-                    Directory.CreateDirectory(RootFolderPath);
-                    CreateTestFolders(RootFolderPath);
+                    RootDirectory.Create();
                 }
                 catch
                 {
@@ -39,12 +36,10 @@ namespace DirectoryApp
                     return false;
                 }
             }
-            Directory.SetCurrentDirectory(RootFolderPath);
             try
             {
-                if (Directory.GetDirectories(RootFolderPath) == null || Directory.GetDirectories(RootFolderPath).Count() == 0)
-                    CreateTestFolders(RootFolderPath);
-
+                if (RootDirectory.GetDirectories() == null || RootDirectory.GetDirectories().Count() == 0)
+                    CreateTestFolders(RootDirectory);
             }
             catch
             {
@@ -52,12 +47,13 @@ namespace DirectoryApp
                 return false;
             }
             Version version;
-            bool isVersion = Version.TryParse(command, out version),
-                isDate = IsDate(command);
+            DateTime date;
+            bool isVersion = Version.TryParse(command, out version);
+            bool isDate = DateTime.TryParse(command, out date) && !isVersion; //подумать!!!!!!!!!!!!!!!!!!!!!!
             string foundFolderName = "";
             if (isVersion)
             {
-                foundFolderName = FindFolderOfVersion(RootFolderPath, version);
+                foundFolderName = FindFolderOfVersion(RootDirectory, version);
                 if (foundFolderName == "")
                 {
                     message = "Folder of version " + version.ToString() + " or previous not found";
@@ -65,103 +61,135 @@ namespace DirectoryApp
                 }
                 else
                 {
-                    foundFolderName = Path.GetFullPath(foundFolderName);
+                    foundFolderName = Path.Combine(RootDirectory.FullName, foundFolderName);
                     message = foundFolderName;
                     return true;
                 }
             }
             if (isDate)
             {
-                DateTime date;
-                DateTime.TryParse(command, out date);
-                foundFolderName = FindFolderOfDate(RootFolderPath, date);
+                foundFolderName = FindFolderOfDate(RootDirectory, date);
                 if (foundFolderName == "")
                 {
                     message = "Folder of date " + command + " or early not found";
                     return false;
                 }
             }
-            if (command.ToLower() == "latest")
-                foundFolderName = FindFolderOfLatestDate(RootFolderPath);
+            if (command.ToLower().Equals("latest"))
+            {
+                foundFolderName = FindFolderOfLatestDate(RootDirectory);
+                if (foundFolderName == "")
+                {
+                    message = "Folder of latest date not found. There are no subdirectories named by date";
+                    return false;
+                }
+            }
             if (foundFolderName == "")
             {
                 message = "Entered command is not valid. Enter version, date or 'Latest' command";
                 return false;
             }
 
-            foundFolderName = Path.GetFullPath(foundFolderName);
+            foundFolderName = Path.Combine(RootDirectory.FullName, foundFolderName);
             message = foundFolderName;
             return true;
         }
 
-        private static void CreateTestFolders(string rootFolderPath)
+        private static void CreateTestFolders(DirectoryInfo rootDirectory)
         {
             Random random = new Random();
             int versionFoldersCount = random.Next(5, 10);
             int dateFoldersCount = random.Next(5, 10);
             for (int i = 0; i < versionFoldersCount; i++)
-                Directory.CreateDirectory(rootFolderPath + random.Next(1, 3).ToString() + "." +
-                    random.Next(0, 10).ToString() + "." + random.Next(0, 10).ToString() + "." +
-                    random.Next(0, 10).ToString());
+                rootDirectory.CreateSubdirectory(String.Join(".", new string[] { random.Next(1, 3).ToString(), random.Next(0, 10).ToString(),
+                    random.Next(0, 10).ToString(), random.Next(0, 10).ToString() }));
             for (int i = 0; i < dateFoldersCount; i++)
-                Directory.CreateDirectory(rootFolderPath + random.Next(2014, 2016).ToString() + "-" +
-                    random.Next(1, 12).ToString() + "-" + random.Next(1, 31).ToString());
+                rootDirectory.CreateSubdirectory(String.Join("-", new string[] { random.Next(2014, 2016).ToString(), random.Next(1, 12).ToString(),
+                    random.Next(1, 31).ToString() }));
         }
 
-        private static string FindFolderOfVersion(string rootFolderPath, Version version)
+        private static string FindFolderOfVersion(DirectoryInfo rootDirectory, Version version)
         {
-            string[] folders = Directory.GetDirectories(rootFolderPath);
-            if (folders == null || folders.Count() == 0)
+            List<DirectoryInfo> subDirs = rootDirectory.GetDirectories().ToList<DirectoryInfo>();
+            if (subDirs == null || subDirs.Count() == 0)
                 return "";
             List<Version> folderVersions = new List<Version>();
-            List<BigInteger> intVersions = new List<BigInteger>();
-            BigInteger multiplier = BigInteger.Pow(10, 10);
             Version v = new Version();
-            for (int i = 0; i < folders.Count(); i++)
-            {
-                folders[i] = Path.GetFileName(folders[i]);
-                if (Version.TryParse(folders[i], out v))
-                {
+            for (int i = 0; i < subDirs.Count(); i++)
+                if (Version.TryParse(subDirs[i].Name, out v))
                     folderVersions.Add(v);
-                    intVersions.Add(ConvertToBigInteger(v));
-                }
-            }
             if (folderVersions.Count == 0)
                 return "";
-            BigInteger intVersion = ConvertToBigInteger(version);
-            BigInteger minDiff = -1;
-            int index = -1;
-            for (int i = 0; i < intVersions.Count; i++)
-            {
-                if (minDiff == -1 && intVersion >= intVersions[i] ||
-                    minDiff != -1 && intVersion >= intVersions[i] && intVersion - intVersions[i] < minDiff)
-                {
-                    minDiff = intVersion - intVersions[i];
-                    index = i;
-                }
-            }
-
-            if (index == -1)
+            
+            bool previousPartIsGreater = false;
+            List<int[]> folderVersionsParts = folderVersions.Select(f => new int[4] { f.Major, f.Minor, f.Build, f.Revision }).ToList<int[]>();
+            int[] versionParts = new int[4] { version.Major, version.Minor, version.Build, version.Revision };
+            versionParts = FindVersion(folderVersionsParts, versionParts, previousPartIsGreater, 0);
+            if (versionParts[0] == -1)
                 return "";
-
-            return folderVersions[index].ToString();
+            else
+            {
+                Version foundVersion;
+                Version.TryParse(String.Join(".", versionParts), out foundVersion);
+                return subDirs[folderVersions.IndexOf(foundVersion)].Name;
+            }
         }
 
-        private static string FindFolderOfDate(string rootFolderPath, DateTime folderDate)
+        private static int[] FindVersion(List<int[]> folderVersionsParts, int[] versionParts, bool previousPartIsGreater, int j)
         {
-            string[] folders = Directory.GetDirectories(rootFolderPath);
-            if (folders == null || folders.Count() == 0)
+            List<int[]> filteredFolderVersionsParts = new List<int[]>(folderVersionsParts);
+            int[] foundVersion = new int[] { -1, -1, -1, -1 };
+            if (j <= 3)
+                if (!previousPartIsGreater)
+                {
+                    int versionPart = versionParts[j];
+                    if (versionPart == -1)
+                        versionPart = 0;
+                    filteredFolderVersionsParts.RemoveAll(f => versionPart < f[j]);
+                    if (filteredFolderVersionsParts.Count == 0)
+                        return foundVersion;
+                    filteredFolderVersionsParts = filteredFolderVersionsParts.OrderBy(f => versionPart - f[j]).ToList<int[]>();
+                    if (j == 3)
+                        return filteredFolderVersionsParts.First();
+                    for (int i = 0; i < filteredFolderVersionsParts.Count; i++)
+                    {
+                        if (versionPart - filteredFolderVersionsParts[i][j] > 0)
+                            previousPartIsGreater = true;
+                        if (foundVersion[0] == -1)
+                            foundVersion = FindVersion(filteredFolderVersionsParts.Where(f => f[j] == filteredFolderVersionsParts[i][j]).ToList<int[]>(), versionParts,
+                                previousPartIsGreater, j + 1);
+                        else
+                            break;
+                    }
+                }
+                else
+                {
+                    filteredFolderVersionsParts = filteredFolderVersionsParts.OrderByDescending(f => f[j]).ToList<int[]>();
+                    if (j == 3)
+                        return filteredFolderVersionsParts.First();
+                    for (int i = 0; i < filteredFolderVersionsParts.Count; i++)
+                        if (foundVersion[0] == -1)
+                            foundVersion = FindVersion(filteredFolderVersionsParts.Where(f => f[j] == filteredFolderVersionsParts[i][j]).ToList<int[]>(), versionParts, previousPartIsGreater, j + 1);
+                        else
+                            break;
+                }
+
+            return foundVersion;
+        }
+
+        private static string FindFolderOfDate(DirectoryInfo rootDirectory, DateTime folderDate)
+        {
+            List<DirectoryInfo> subDirs = rootDirectory.GetDirectories().ToList<DirectoryInfo>();
+            if (subDirs == null || subDirs.Count() == 0)
                 return "";
 
             int index = -1;
-            List<DateTime> folderDates = new List<DateTime>();
-            DateTime date = new DateTime(),
-                foundDate = new DateTime();
+            DateTime date = new DateTime();
+            DateTime foundDate = new DateTime();
             TimeSpan dateDiff = TimeSpan.MaxValue;
-            for (int i = 0; i < folders.Count(); i++)
+            for (int i = 0; i < subDirs.Count(); i++)
             {
-                folders[i] = Path.GetFileName(folders[i]);
-                if (DateTime.TryParse(folders[i], out date) && folderDate >= date && folderDate - date < dateDiff)
+                if (DateTime.TryParse(subDirs[i].Name, out date) && folderDate >= date && folderDate - date < dateDiff)
                 {
                     foundDate = date;
                     index = i;
@@ -170,43 +198,28 @@ namespace DirectoryApp
             }
             if (index == -1)
                 return "";
-            return foundDate.ToString(dateFormat);
+            return subDirs[index].Name;
         }
 
-        private static string FindFolderOfLatestDate(string rootFolderPath)
+        private static string FindFolderOfLatestDate(DirectoryInfo rootDirectory)
         {
-            string[] folders = Directory.GetDirectories(rootFolderPath);
-            if (folders == null || folders.Count() == 0)
+            List<DirectoryInfo> subDirs = rootDirectory.GetDirectories().ToList<DirectoryInfo>();
+            if (subDirs == null || subDirs.Count() == 0)
                 return "";
 
             DateTime d = new DateTime();
             DateTime latestDate = DateTime.MinValue;
-            int index = 0;
-            for (int i = 0; i < folders.Count(); i++)
-            {
-                folders[i] = Path.GetFileName(folders[i]);
-                if (DateTime.TryParse(folders[i], out d) && d > latestDate)
+            int index = -1;
+            for (int i = 0; i < subDirs.Count(); i++)
+                if (DateTime.TryParse(subDirs[i].Name, out d) && d > latestDate)
                 {
                     latestDate = d;
                     index = i;
                 }
-            }
-
-            return folders[index];
+            if (index == -1)
+                return "";
+            return subDirs[index].Name;
         }
-
-        private static BigInteger ConvertToBigInteger(Version v)
-        {
-            BigInteger multiplier = BigInteger.Pow(10, 10);
-            return BigInteger.Pow(multiplier, 4) * v.Major + BigInteger.Pow(multiplier, 3) * v.Minor +
-                         BigInteger.Pow(multiplier, 2) * v.Build + BigInteger.Pow(multiplier, 1) * v.Revision +
-                         v.MajorRevision * multiplier + v.MinorRevision;
-        }
-
-        public static bool IsDate(string input)
-        {
-            DateTime date;
-            return DateTime.TryParse(input, out date) && input.Length <= 11;
-        }
+        
     }
 }
